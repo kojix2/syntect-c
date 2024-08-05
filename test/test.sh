26360ba341fdc75d93b11d760aac3172e58d32b0
kojix2 <2xijok@gmail.com>
2024-08-05 15:36:22 +0900

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,10 @@
 
 # Add missing import
 export LD_LIBRARY_PATH="../target/release:../target/debug"
+# MacOS
+export DYLD_LIBRARY_PATH="../target/debug"
 
 # Compile the C test file
 gcc test.c -L ../target/release -L ../target/debug -lsyntect_c -o test
 
+./test
